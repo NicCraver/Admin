1b2ac34c9a978fcb58252352b5c891b811ab1208
Nic <951477037@qq.com>
2019-07-05 10:05:15 +0800

    Nic

---
+++ vue.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  baseUrl: "/", //根路径
+  outputDir: "dist", //构建输出目录
+  lintOnSave: false,
+  devServer: {
+    open: true,
+    host: "localhost",
+    port: 9721,
+    https: false,
+    hotOnly: false
+    // proxy: {
+    //   //配置跨域
+    //   "/api": {
+    //     target: "http://localhost:5000/api/",
+    //     ws: true,
+    //     changeOrigin: true,
+    //     pathRewrite: {
+    //       "^/api": ""
+    //     }
+    //   },
+    //   before(app) {
+    //     app.get("/api/goods", (req, res) => {
+    //       res.json();
+    //     });
+    //   }
+    // }
+  }
+};
