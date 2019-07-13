349e390b6b1cb411db3fa9f172122a17b2169ab9
Nic <951477037@qq.com>
2019-07-13 09:39:37 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -3,17 +3,21 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store/index";
-import "@/styles/index.scss"; // global css
 import ElementUI from "element-ui";
+
+import "@/styles/index.scss"; // global css
 import "element-ui/lib/theme-chalk/index.css";
 import "./promission";
-Vue.config.productionTip = false;
+import PageHeader from '@/components/PageHeader/index.js'//引用全局组件PageHeader
+
 import api from "./api"; // 导入api接口
 Vue.prototype.$api = api; // 将api挂载到vue的原型上
-import PageHeader from '@/components/PageHeader/index.js'//引用全局组件Button
-Vue.use(PageHeader);//使用全局组件Button
 
+Vue.config.productionTip = false;
+
+Vue.use(PageHeader);//使用全局组件PageHeader
 Vue.use(ElementUI);
+
 new Vue({
   router,
   store,
